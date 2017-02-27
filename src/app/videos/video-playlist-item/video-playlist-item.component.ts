9658ecba0b95d453c4ce42ab69067d65b92eb3b0
Porawit Poboonma <ball6847@gmail.com>
2017-02-27 21:36:47 +0700

    add dummy playlist components

---
+++ src/app/videos/video-playlist-item/video-playlist-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dl-video-playlist-item',
+  templateUrl: 'video-playlist-item.component.html',
+  styleUrls: ['video-playlist-item.component.css']
+})
+export class VideoPlaylistItemComponent {
+
+}
---
+++ src/app/videos/video-playlist/video-playlist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dl-video-playlist',
+  templateUrl: 'video-playlist.component.html',
+  styleUrls: ['video-playlist.component.css']
+})
+export class VideoPlaylistComponent {
+
+}
