d4aee2aa0036ae9152853b0afb4be55daf7a05ef
Porawit Poboonma <ball6847@gmail.com>
2017-03-06 15:36:42 +0700

    use ISearchResultState instead

--- src/app/shared/nav/search-box/search-box.component.ts
+++ src/app/shared/nav/search-box/search-box.component.ts
@@ -1,9 +1,8 @@
 import { Component } from "@angular/core";
 import { Store } from "@ngrx/store";
 import { Subject } from "rxjs/Subject";
-import { AppState } from "../../../app.store";
 import { Video, VideoService } from "../../../video";
-import { SearchResultAction } from "../../../search/store";
+import { SearchResultAction, ISearchResultState } from "../../../search/store";
 
 @Component({
   selector: "search-box",
@@ -13,7 +12,7 @@ import { SearchResultAction } from "../../../search/store";
 export class SearchBoxComponent {
   searchTerm$ = new Subject<string>();
 
-  constructor(private store: Store<AppState>, private videoService: VideoService) {}
+  constructor(private store: Store<ISearchResultState>, private videoService: VideoService) {}
 
   ngOnInit() {
     this.videoService.rxSearch(this.searchTerm$)
