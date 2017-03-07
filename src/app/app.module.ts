7d4d883dcb07d0bb916ddcca8575ee20b0dfadfd
Porawit Poboonma <ball6847@gmail.com>
2017-03-07 14:07:56 +0700

    use github:ball6847/ng2-youtube-player instead

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { YoutubePlayerModule } from 'ng2-youtube-player';
+import { YoutubePlayerModule } from 'ng2-youtube-player/src';
 import { Ng2PaginationModule } from 'ng2-pagination';
 import { DragulaModule } from 'ng2-dragula/ng2-dragula';
 import { DropdownModule, PopoverModule } from 'ng2-bootstrap';
